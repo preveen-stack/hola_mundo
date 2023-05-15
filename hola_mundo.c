dc923fa5d9ba85c3378de17c6092dca9d857ba82
Preveen P <31464911+preveen-stack@users.noreply.github.com>
2023-05-15 14:17:08 +0530

    Update hola_mundo.c

--- hola_mundo.c
+++ hola_mundo.c
@@ -1,6 +1,8 @@
 // Programa de ejemplo: ¡Hola, Mundo!
 // Este programa muestra un mensaje de saludo en español.
 
+#include "palabras_clave.h"
+
 entero funcion_principal() {
     imprimir("¡Hola, mundo!\n");
     retornar 0;
