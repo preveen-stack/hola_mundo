fed76949dc743d8aaaf1e35e1af160e8489e76b6
Preveen P <31464911+preveen-stack@users.noreply.github.com>
2023-05-15 14:12:20 +0530

    hola_mundo.c

---
+++ hola_mundo.c
@@ -0,0 +1,7 @@
+// Programa de ejemplo: ¡Hola, Mundo!
+// Este programa muestra un mensaje de saludo en español.
+
+entero funcion_principal() {
+    imprimir("¡Hola, mundo!\n");
+    retornar 0;
+}
